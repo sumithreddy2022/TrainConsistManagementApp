7202149b9375f0afa1c4142650de06c1e78b3fd9
sumithreddy2022 <sumithreddy202@gmail.com>
2026-03-25 08:52:40 +0530

    UC1: Add welcome message module

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainConsistManagementApp {
-}
+    public static void main(String[] args) {
+        System.out.println("=== Train Consist Management App ===");
+        List<String> consist = new ArrayList<>();
+        System.out.println("Initial bogie count: " + consist.size());
+    }
+ }
